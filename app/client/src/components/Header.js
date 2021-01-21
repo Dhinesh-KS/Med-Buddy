d4df2a878f3a3a356f3959dc0292054d43c466bc
Dhinesh KS <dhinaksd@gmail.com>
2021-01-22 00:33:44 +0530

    Layout setup

---
+++ app/client/src/components/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { AppBar, Toolbar, Grid, InputBase, IconButton, Badge, makeStyles } from "@material-ui/core";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#fff",
+    transform: "translatez(0)",
+  },
+  searchInput: {
+    opacity: "0.6",
+    color: "#000",
+    padding: `0px ${theme.spacing(1)}px`,
+    fontSize: "0.8rem",
+    border: "solid 1px #efefef",
+    borderRadius: "1rem",
+    backgroundColor: "#daddef",
+    "&:hover": {
+      border: "solid 1px #969696",
+    },
+    "& .MuiSvgIcon-root": {
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <Grid container alignItems="center">
+          <Grid item>
+            <InputBase
+              placeholder="Search ..."
+              className={classes.searchInput}
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
+          </Grid>
+          <Grid item sm></Grid>
+          <Grid item>
+            <IconButton>
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsNoneIcon fontSize="small" />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={3} color="primary">
+                <ChatBubbleOutlineIcon fontSize="small" />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <PowerSettingsNewIcon fontSize="small" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
---
+++ app/client/src/components/SideBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  sideMenu: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    left: "0px",
+    width: "20%",
+    height: "100%",
+    backgroundColor: "#253053",
+  },
+  pageTitle: {
+    paddingLeft: theme.spacing(3),
+    paddingTop: theme.spacing(3),
+    "& .MuiTypography-subtitle2": {
+      opacity: "0.6",
+    },
+  },
+}));
+
+function SideMenu(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.sideMenu}>
+      <div className={classes.pageTitle}>
+        <Typography variant="h6" component="div" style={{ color: "#fff" }}>
+          Med-Buddy
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          component="div"
+          style={{ color: "#fff", fontWeight: "lighter" }}
+        >
+          Serve you the Best
+        </Typography>
+      </div>
+    </div>
+  );
+}
+
+export default SideMenu;
